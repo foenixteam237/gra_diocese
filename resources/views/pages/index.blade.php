54c146db8e4011474184b8f9c2e1f2170abf6bff
YvesFranky <yvesfrankynyomaombala@gmail.com>
2023-11-20 08:28:01 +0100

    Archidiocese

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Archidiocese de Garoua</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <ul>
+                <li>Acceuil</li>
+                <li>Arch</li>
+                <li>Acceuil</li>
+                <li>Acceuil</li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
